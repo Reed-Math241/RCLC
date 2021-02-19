146880e4cb4b56844d3598ecea37db677b85a9af
Goodernews <tbogoodnews@gmail.com>
2021-02-19 12:24:13 -0800

    documented

--- rmd/name_clean.py
+++ rmd/name_clean.py
@@ -6,13 +6,13 @@ nltk.download('punkt')
 
 
 def clean_str(s0):
-  tagged = nltk.pos_tag(nltk.word_tokenize(s0))
-  return " ".join([word[0] for word in tagged if word[1]=="NNP"])
+  tagged = nltk.pos_tag(nltk.word_tokenize(s0)) #Tags parts of speech
+  return " ".join([word[0] for word in tagged if word[1]=="NNP"]) #extraxts proper nouns
 
 def name_clean(authors):
-  return [clean_str(mess) if mess == mess else "" for mess in authors]
+  return [clean_str(mess) if mess == mess else "" for mess in authors] #applies to all values
 
-df = pd.read_csv("data/RCLC.csv") 
-df["Author"] = name_clean(df["Author"])
+df = pd.read_csv("data/RCLC.csv") #reads
+df["Author"] = name_clean(df["Author"]) #applies
 
-df.to_csv("data/RCLC.csv", encoding='utf-8', index=False)
+df.to_csv("data/RCLC.csv", encoding='utf-8', index=False) #writes
