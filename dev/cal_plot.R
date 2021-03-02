3d5eaa1e4c54749c15f2a5aca7ab654e41e035e3
Goodernews <tbogoodnews@gmail.com>
2021-03-01 16:19:18 -0800

    Changed file locations

--- dev/cal_plot.R
+++ dev/cal_plot.R
@@ -1,6 +1,6 @@
 
 # Script to generate calendar graphic.
-musing_RL <- read_csv("../data/RCLC.csv")
+musing_RL <- read_csv("../dev/RCLC.csv")
 musing_RL$Loaned <- as.Date(musing_RL$Loaned,"%y-%m-%d")
 musing_RL <- musing_RL[!as.numeric(strftime(musing_RL$Loaned, "%m")) %in% 6:8,]
 
